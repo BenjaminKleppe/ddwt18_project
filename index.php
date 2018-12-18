6947dbfc7c83fe5d288a5302f28778c80157eaba
Benjamin <benjaminkleppe@hotmail.com>
2018-12-18 13:50:23 +0100

    right index

--- index.php
+++ index.php
@@ -14,52 +14,49 @@ $db = connect_db('localhost', 'ddwt_project', 'ddwt18','ddwt18');
 $template = Array(
     1 => Array(
         'name' => 'Home',
-        'url' => '/ddwt18_project/'
-    ),
+        'url' => '/ddwt18_project/'),
     2 => Array(
         'name' => 'Overview',
-        'url' => '/ddwt18_project/overview/'
-    ),
+        'url' => '/ddwt18_project/overview/'),
     3 => Array(
         'name' => 'Add series',
-        'url' => '/ddwt18_project/add/'
-    ),
+        'url' => '/ddwt18_project/add/'),
     4 => Array(
         'name' => 'My Account',
-        'url' => '/ddwt18_project/myaccount/'
-    ),
+        'url' => '/ddwt18_project/myaccount/'),
     5 => Array(
         'name' => 'Register',
-        'url' => '/ddwt18_project/register/'
-    ));
+        'url' => '/ddwt18_project/register/')
+);
 
 /* Overview page */
-elseif (new_route('/DDWT18/ddwt18_project/overview/', 'get')) {
+if (new_route('/DDWT18/ddwt18_project/overview/', 'get')) {
     /* Page info */
-$page_title = 'Overview';
-$breadcrumbs = get_breadcrumbs([
-    'DDWT18' => na('/DDWT18/', False),
-    'Week 2' => na('/DDWT18/ddwt18_project/', False),
-    'Overview' => na('/DDWT18/ddwt18_project/overview', True)
-]);
-$navigation = get_navigation($template, '2');
+    $page_title = 'Overview';
+    $breadcrumbs = get_breadcrumbs([
+        'DDWT18' => na('/DDWT18/', False),
+        'Week 2' => na('/DDWT18/ddwt18_project/', False),
+        'Overview' => na('/DDWT18/ddwt18_project/overview', True)
+    ]);
+    $navigation = get_navigation($template, '2');
 
-/* Page content */
-$page_subtitle = 'The overview of all rooms available';
-$page_content = 'On this page you will find all available rooms for internationals.';
-$left_content = get_room_table(get_rooms($db), $db);
+    /* Page content */
+    $page_subtitle = 'The overview of all rooms available';
+    $page_content = 'On this page you will find all available rooms for internationals.';
+    $left_content = get_room_table(get_rooms($db), $db);
+    print_r('hi');
 
-/* Get error msg from POST route */
-if ( isset($_GET['error_msg']) ) { $error_msg = get_error($_GET['error_msg']); }
-
-/* Choose Template */
-include use_template('main');
+    /* Choose Template */
+    include use_template('main');
 }
+
 
 /*
 else {
     http_response_code(404);
 }
 */
+
+
 
 
