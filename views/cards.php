f0ab95cd1bfa138d09259322a440c0c8a60901d8
ThomasTan1998 <T.C.Tan@student.rug.nl>
2018-12-20 15:20:55 +0100

    added cards.php

---
+++ views/cards.php
@@ -0,0 +1,23 @@
+<!-- Series count -->
+<div class="card">
+    <div class="card-header">
+        Rooms
+    </div>
+    <div class="card-body">
+        <p class="count">Room overview already has</p>
+        <h2><?= $nbr_rooms ?></h2>
+        <p>rooms listed</p>
+        <a href="/DDWT18/ddwt18_project/add/" class="btn btn-primary">List yours</a>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header">
+        Users
+        </div>
+    <div class="card-body">
+        <p class="count">Room overview already has</p>
+        <h2><?= $nbr_users ?></h2>
+        <p>active users</p>
+        <a href="/DDWT18/ddwt18_project/register/" class="btn btn-primary">Join now</a>
+    </div>
+</div>
