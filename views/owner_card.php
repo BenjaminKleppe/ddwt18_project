8d8cb84dca4594b477390aaefbfd42d1f65fc95a
ThomasTan1998 <T.C.Tan@student.rug.nl>
2019-01-09 15:49:03 +0100

    commitje

--- views/owner_card.php
+++ views/owner_card.php
@@ -1,30 +1,30 @@
 <?php if ($disp_buttons) { ?>
-<div class="card">
-    <div class="card-header">
-        Owner
-    </div>
-    <div class="card-body">
-        <div class="ml-4 pb-4"><h2><?= $added_by ?></h2></div>
-        <div>
-            <div class="pb-4" style="text-align: center"><?= implode(" ", $profilepicture) ?></div>
+    <div class="card">
+        <div class="card-header">
+            Owner
         </div>
-        <div class="col-md-12 col-s-12 col-sm-12 col-xs-12 pb-4 pl-2">
-            <div class="col-md-6 col-sm-6 col-s-6 col-xs-6">
-                <p><strong>Birth Date: </strong></p>
-                <p><strong>Language: </strong></p>
-                <p><strong>Email: </strong></p>
-                <p><strong>Phone number: </strong></p>
+        <div class="card-body">
+            <div class="ml-4 pb-4"><h2><?= $added_by ?></h2></div>
+            <div>
+                <div class="pb-4" style="text-align: center"><?= implode(" ", $profilepicture) ?></div>
             </div>
-            <div class="col-md-6 col-sm-6 col-s-6 col-xs-6">
-                <p><?= $birthdate ?></p>
-                <p><?= $language ?></p>
-                <p><?= $email ?></p>
-                <p><?= $phonenumber ?></p>
+            <div class="col-md-12 col-s-12 col-sm-12 col-xs-12 pb-4 pl-2">
+                <div class="col-md-6 col-sm-6 col-s-6 col-xs-6">
+                    <p><strong>Birth Date: </strong></p>
+                    <p><strong>Language: </strong></p>
+                    <p><strong>Email: </strong></p>
+                    <p><strong>Phone number: </strong></p>
+                </div>
+                <div class="col-md-6 col-sm-6 col-s-6 col-xs-6">
+                    <p><?= $birthdate ?></p>
+                    <p><?= $language ?></p>
+                    <p><?= $email ?></p>
+                    <p><?= $phonenumber ?></p>
+                </div>
+                <p><a href="<?= $ownerlink ?>" role="button" class="col-md-6 col-sm-6 col-s-6 btn btn-primary ml-4 mt-4">Owner info</a></p>
             </div>
-            <p><a href="<?= $ownerlink ?>" role="button" class="col-md-6 col-sm-6 col-s-6 btn btn-primary ml-4 mt-4">Owner info</a></p>
         </div>
     </div>
-</div>
 <?php } ?>
 <?php if ($display_buttons) { ?>
 <div class="card">
