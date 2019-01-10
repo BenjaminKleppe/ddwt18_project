8687d5eecbd04acceaf7f5fa70cf0ef777044230
Gijs <g.danoe@student.rug.nl>
2019-01-10 01:02:44 +0100

    css

--- views/contact.php
+++ views/contact.php
@@ -76,7 +76,7 @@
                 </div>
                 <?php if ($display_buttons) {} ?>
                 <input type="hidden" value="<?= $room_id ?>" name="room_id">
-                <button type="submit" id="add" class="btn btn-primary">Opt-in</button>
+                <button type="submit" id="suc" class="btn btn-success">Opt in</button>
             </form>
         </div>
     </div>
